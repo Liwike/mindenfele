047dcfb163006b962318a45d7e01ff0080950316
Rontó, Zsolt (FT D EU HU OPS 2 HMI 1) <zsolt.ronto@evosoft.com>
2026-03-13 08:23:48 +0100

    teszt.py a repo-ba

--- teszt.py
+++ teszt.py
@@ -1,5 +1,8 @@
 import torch
 
+# pip uninstall torch torchvision torchaudio -y
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+
 print(torch.cuda.is_available())
 print(torch.version.cuda)
 print(torch.cuda.get_device_name(0))
